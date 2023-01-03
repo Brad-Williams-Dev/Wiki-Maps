0afe2588bc55afa7b247b2f13fe5451dab0c85d3
Brad Williams <bradley_williams2003@hotmail.com>
2023-01-03 18:14:22 -0400

    Fix some styles

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,35 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for (user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+$(document).ready(function() {
+  const initMap = function(lat, lng, mapId) {
+    const zoom = 12;
+    const lat_long_Ottawa = { lat: lat, lng: lng };
+
+    const coordsOttawa = {
+      zoom: zoom,
+      center: lat_long_Ottawa,
+    };
+    const map = new google.maps.Map(
+      document.getElementById(mapId),
+      coordsOttawa
+    );
+
+    const addMarker = (coords, map) => {
+      const marker = new google.maps.Marker({
+        position: coords,
+        map: map,
+      });
+    };
+
+    addMarker(lat_long_Ottawa, map);
+  };
+
+  const maps = $(".map-class");
+
+  maps.each(function(index) {
+    const mapId = $(this).attr("id");
+    const mapLat = $(this).attr("data-lon");
+    const mapLon = $(this).attr("data-lat");
+    initMap(Number(mapLat), Number(mapLon), mapId);
+  });
+
+
 });
